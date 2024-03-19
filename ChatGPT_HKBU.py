c116ce073c8ff1cb43e5fc33594ca1d62dca3d93
glocas <z1114rnizh@163.com>
2024-03-19 15:14:59 +0800

    modify config method

--- ChatGPT_HKBU.py
+++ ChatGPT_HKBU.py
@@ -1,20 +1,22 @@
-import configparser
+# import configparser
 import requests
+import os
 
 class HKBU_ChatGPT():
     def __init__(self,config_='./config.ini'):
-        if type(config_) == str:
-            self.config = configparser.ConfigParser()
-            self.config.read(config_)
-        elif type(config_) == configparser.ConfigParser:
-            self.config = config_
+        # if type(config_) == str:
+        #     self.config = configparser.ConfigParser()
+        #     self.config.read(config_)
+        # elif type(config_) == configparser.ConfigParser:
+        #     self.config = config_
+        pass
 
     def submit(self,message):
         conversation = [{"role": "user", "content": message}]
 
-        url = (self.config['CHATGPT']['BASICURL']) + "/deployments/" + (self.config['CHATGPT']['MODELNAME']) + "/chat/completions/?api-version=" + (self.config['CHATGPT']['APIVERSION'])
+        url = (os.environ['BASICURL']) + "/deployments/" + (os.environ['MODELNAME']) + "/chat/completions/?api-version=" + (os.environ['APIVERSION'])
 
-        headers = { 'Content-Type': 'application/json', 'api-key': (self.config['CHATGPT']['ACCESS_TOKEN']) }
+        headers = { 'Content-Type': 'application/json', 'api-key': (os.environ['GPT_ACCESS_TOKEN']) }
         payload = { 'messages': conversation }
         response = requests.post(url, json=payload, headers=headers)
         if response.status_code == 200:
@@ -24,7 +26,7 @@ class HKBU_ChatGPT():
             return 'Error:', response
 
 if __name__ == '__main__':
-    ChatGPT_test = HKBU_ChatGPT()   
+    ChatGPT_test = HKBU_ChatGPT()
 
     while True:
         user_input = input("Typing anything to ChatGPT:\t")
--- chatbot.py
+++ chatbot.py
@@ -4,7 +4,8 @@
 from telegram import Update
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
 CallbackContext)
-import configparser
+import os
+# import configparser
 import logging
 import redis
 from ChatGPT_HKBU import HKBU_ChatGPT
@@ -12,14 +13,15 @@ from ChatGPT_HKBU import HKBU_ChatGPT
 global redis1
 def main():
 	# Load your token and create an Updater for your Bot
-	config = configparser.ConfigParser()
-	config.read('config.ini')
-	updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+	# config = configparser.ConfigParser()
+	# config.read('config.ini')
+	# updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+	updater = Updater(token=(os.environ['ACCESS_TOKEN']),use_context=True)
 	dispatcher = updater.dispatcher
 	global redis1
-	redis1 = redis.Redis(host=(config['REDIS']['HOST']),
-						 password=(config['REDIS']['PASSWORD']),
-						 port=(config['REDIS']['REDISPORT']))
+	redis1 = redis.Redis(host=(os.environ['HOST']),
+						 password=(os.environ['PASSWORD']),
+						 port=(os.environ['REDISPORT']))
 
 	# You can set this logging module,
 	# so you will know when and why things do not work as expected
@@ -31,7 +33,8 @@ def main():
 
 	# dispatcher for chatgpt
 	global chatgpt
-	chatgpt = HKBU_ChatGPT(config)
+	# chatgpt = HKBU_ChatGPT(config)
+	chatgpt = HKBU_ChatGPT()
 	chatgpt_handler = MessageHandler(Filters.text & (~Filters.command), equiped_chatgpt)
 	dispatcher.add_handler(chatgpt_handler)
 
